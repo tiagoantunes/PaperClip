62f8177f709a51924e32355b7323990b11baead6
Tiago Antunes <tiagoantunes7@gmail.com>
2025-02-16 16:54:22 +0000

    Initial Commit

---
+++ PaperClip/PaperClipApp.swift
@@ -0,0 +1,17 @@
+//
+//  PaperClipApp.swift
+//  PaperClip
+//
+//  Created by Tiago Antunes on 16/02/2025.
+//
+
+import SwiftUI
+
+@main
+struct PaperClipApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ PaperClipTests/PaperClipTests.swift
@@ -0,0 +1,17 @@
+//
+//  PaperClipTests.swift
+//  PaperClipTests
+//
+//  Created by Tiago Antunes on 16/02/2025.
+//
+
+import Testing
+@testable import PaperClip
+
+struct PaperClipTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
